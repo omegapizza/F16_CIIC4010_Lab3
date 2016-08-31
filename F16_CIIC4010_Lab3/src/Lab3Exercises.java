9a8ca1caddb78795bbc3b3dd3aac60046804da2e
4015b24 <4015b24@toronja.ece.uprm.edu>
2016-08-31 10:11:48 -0400

    The changes made were the ones in the instructions of the lab

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Noel Valentin Roman");
+		JFrame myFrame = new JFrame("Noel E. Valentin Roman");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(875, 400);
 		myFrame.setSize(200, 200);
